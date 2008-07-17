3b6106df0840d9ef003e064a8c468b944d227352
tehriddler <tehriddler@0b404c34-2b52-0410-825b-3d5fbaf8d42e>
2008-07-17 01:53:54 +0000

    Small edit to format and needed information.

--- index.php
+++ index.php
@@ -1,31 +1,28 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
-   "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title>Wonder Game</title>
-	<meta name="generator" content="TextMate http://macromates.com/">
 	<meta name="author" content="Ethan Mick">
-	<!-- Date: 2008-06-30 -->
- <link rel="stylesheet" type="text/css" href="http://wondergame.freehostia.com/open/MainDesign.css" />
+ <link rel="stylesheet" type="text/css" href="/open/MainDesign.css" />
 </head>
 <body>
         <div id="main">
-        <div id="header"><img src="http://wondergame.freehostia.com/open/wondermainlogo.gif"</img>
+        <div id="header"><img src="/open/wondermainlogo.gif"</img>
         </div>
         <div id="leftbar">
 	<p>Welcome to</p> <h1>Wonder</h1> <p>the game of economic Domination.</p>
 	<p>Because Wonder is the in the Pre-alpha stages, only people who have a code may sign up.  This is to keep the riff-raff out while we prepare the game for the masses.  This site will be updated as soon as the internal workings of the game are complete.</p>
 	<p>Thank you for your cooperation.</p>
-        <p>If you are curious about this game and want more information, we have set up a <a href="http://wondergame.freehostia.com/blog/">blog</a>, and it has a <a href="http://wondergame.freehostia.com/blog/?page_id=4">Change log</a> for recent changes.</p>
+        <p>If you are curious about this game and want more information, we have set up a <a href="http://wondergame.freehostia.com/blog/">blog</a>, and it has a <a href="/blog/?page_id=4">Change log</a> for recent changes.</p>
         </div>
         <div id="rightbar">
-	<form action="http://wondergame.freehostia.com/open/WonderLogin.php" name="userlogin" method="post">
+	<form action="/open/WonderLogin.php" name="userlogin" method="post">
 	Username: <input type="text" name="username" id=""><br />
 	Password: <input type="password" name="password" id=""><br />
 	<input type="submit" value="Login"><br />
 	<br />
-	If you have a testing code, Redeem it <a href="http://wondergame.freehostia.com/open/register.php">here.</a>
+	If you have a testing code, Redeem it <a href="/open/register.php">here.</a>
 </div>
 </body>
 </html>
