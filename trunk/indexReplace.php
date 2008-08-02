0bd596040e144d8671a69c023aa6ffc07767c7bb
tehriddler <tehriddler@0b404c34-2b52-0410-825b-3d5fbaf8d42e>
2008-08-02 01:44:21 +0000

    Minor changes along with adding files for login system.

--- trunk/indexReplace.php
+++ trunk/indexReplace.php
@@ -1,5 +1,5 @@
 <?php
-require "header.php";
+require_once "header.php";
 /** 
  * Login System for Online user access
  * 
@@ -9,29 +9,6 @@ require "header.php";
 /**
  * Start Login.
  */
-if(!IsLoggedIn())
-{
-?>
-<table border="0">
-    <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
-        <tr>
-            <td>Username: </td>
-            <td><input type="text" size="32" name="user"></td>
-        </tr>
-        <tr>
-            <td>Password: </td>
-            <td><input type="password" size="12" name="pass"></td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td><input type="submit" name="submit" value="Login"></td>
-        </tr>
-    </form>
-</table>
-<br />
-<a href="reg.php">Register Here</a>
-<?php
-}
 if(isset($_POST['submit']))
 {
     // Set veriables.
@@ -40,6 +17,15 @@ if(isset($_POST['submit']))
 	
     // Call verification function.
     loginScriptVerify($user, $pass);
+}
+if(!IsLoggedIn())
+{
+    DispLoginBox();
+    echo "<br /><a href=\"reg.php\">Register Here</a>";
+} else {
+    
+    echo "Welcome!";
+    
 }
 /**
  * End Login.
